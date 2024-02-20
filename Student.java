c559f38b36ff6f96855df0c8ec43eb832fb0c45e
seema <01fe22bca169@kletech.ac.in>
2024-02-20 10:54:35 +0530

    code for student

---
+++ Student.java
@@ -0,0 +1,22 @@
+ public class Student {
+    String name;
+    String department;
+
+    public void displayName() {
+        System.out.println("--------------------------------");
+        System.out.println("Student Name: " + name);
+        System.out.println("Student department: " + department);
+        System.out.println("--------------------------------");
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.name = "Seema morab";
+        s.department = "BCA";
+        s.displayName();
+        //System.out.println("THE COURCES REGISTERED ARES:");
+        //StudentCourses studentCourses = new StudentCourses();
+        //studentCourses.displayStudentMarks();
+
+    }
+}
